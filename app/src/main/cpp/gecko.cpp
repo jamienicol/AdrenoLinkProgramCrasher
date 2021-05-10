11239b342847630c91d9329a94e1cba64b1b0a79
Jamie Nicol <jamie@jamienicol.me>
2021-05-10 12:50:25 +0100

    Lots of smaller mmaps - DOES CRASH

--- app/src/main/cpp/gecko.cpp
+++ app/src/main/cpp/gecko.cpp
@@ -6,8 +6,8 @@
 extern "C" void Java_me_jamienicol_adrenolinkprogramcrasher_Gecko_init(JNIEnv *env, jobject thiz) {
     __android_log_write(ANDROID_LOG_INFO, "JAMIE", "Gecko init()");
 
-    for (int i = 0; i < 100; i++) {
-        size_t map_len = 91184349;
+    for (int i = 0; i < 1000; i++) {
+        size_t map_len = 1024 * 1024;
         __android_log_print(ANDROID_LOG_INFO, "JAMIE",
                             "mmapping %zu bytes", map_len);
         void *map = mmap(0, map_len, PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
