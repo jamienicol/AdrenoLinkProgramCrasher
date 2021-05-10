ed0816e7f9234eaf8626f05322d8b9b8262e604d
Jamie Nicol <jamie@jamienicol.me>
2021-05-10 12:45:26 +0100

    100 mmaps DOES NOT CRASH

--- app/src/main/cpp/gecko.cpp
+++ app/src/main/cpp/gecko.cpp
@@ -1,6 +1,24 @@
 #include <jni.h>
 #include <android/log.h>
+#include <sys/mman.h>
+#include <errno.h>
 
 extern "C" void Java_me_jamienicol_adrenolinkprogramcrasher_Gecko_init(JNIEnv *env, jobject thiz) {
     __android_log_write(ANDROID_LOG_INFO, "JAMIE", "Gecko init()");
+
+    for (int i = 0; i < 100; i++) {
+        size_t map_len = 91184349;
+        __android_log_print(ANDROID_LOG_INFO, "JAMIE",
+                            "mmapping %zu bytes", map_len);
+        void *map = mmap(0, map_len, PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+        if (map == MAP_FAILED) {
+            __android_log_print(ANDROID_LOG_ERROR, "JAMIE", "mmap failed: %d\n",
+                                errno);
+            break;
+        } else {
+            __android_log_print(ANDROID_LOG_INFO, "JAMIE",
+                                "mmap succeeded: 0x%zx - 0x%zx\n", (size_t) map,
+                                (size_t) map + map_len);
+        }
+    }
 }
